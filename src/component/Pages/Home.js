5f9963155870dc730c74e5898dd1926ad52c34b3
vannuthchhorn <vannuthchhorn@gmail.com>
2022-03-27 14:07:45 +0700

    pop up

--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -6,6 +6,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import CardActions from "@material-ui/core/CardActions";
+import Popup from 'reactjs-popup';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,7 +27,7 @@ const classes = useStyles();
 return (
 	<div style={{ width: '99%', backgroundColor: '#E6E6FA',padding: '10px' }}>
     <Grid container spacing={3}>
-      {/* ++++++++++++++++++++++++++++++++++++++++++++++ */}
+      {/* ===============================================================*/}
       <Grid item xs={6} sm={3}>
       <div style={{}}>
         <Card
@@ -45,7 +46,7 @@ return (
               Greetings of the day
             </Typography>
             <Typography variant="h5" component="h2">
-              How are you ?
+              359$
             </Typography>
             <Typography
               style={{
@@ -58,13 +59,71 @@ return (
               Stay Happy
             </Typography>
           </CardContent>
-          <CardActions>
-            <Button size="small">Stay Safe.....</Button>
-          </CardActions>
+          <Popup
+            trigger={<button className="button"> Open Modal </button>}
+            modal
+            nested
+          >
+            {close => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <Grid item xs={6} sm={3}>
+                  <div style={{}}>
+                    <Card
+                      style={{
+                      width: 350,
+                      backgroundColor: "orange",
+                      }}
+                    >
+                      <CardContent>
+                        <Typography
+                          style={{ fontSize: 14 }}
+                          color="textSecondary"
+                          gutterBottom
+                        >
+                          {/* <img src={oppo_Reno5} className="App-logo" alt="oppo_Reno5" /> */}
+                          Greetings of the day
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                          450$
+                        </Typography>
+                        <Typography
+                          style={{
+                          marginBottom: 12,
+                          }}
+                          color="textSecondary"
+                        >
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                          Stay Happy
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small">Add to Card</Button>
+                      </CardActions>
+                    </Card>
+                    </div>
+                  </Grid>
+                <div className="actions">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
         </Card>
         
         </div>
-        {/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+        {/* ===============================================================*/}
       </Grid>
       <Grid item xs={6} sm={3}>
       <div style={{}}>
@@ -84,7 +143,7 @@ return (
               Greetings of the day
             </Typography>
             <Typography variant="h5" component="h2">
-              How are you ?
+              450$
             </Typography>
             <Typography
               style={{
@@ -98,13 +157,12 @@ return (
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Stay Safe.....</Button>
+            <Button size="small">Add to Card</Button>
           </CardActions>
         </Card>
-        
         </div>
       </Grid>
-      {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+      {/* ===============================================================*/}
       <Grid item xs={6} sm={3}>
       <div style={{}}>
         <Card
@@ -123,7 +181,7 @@ return (
               Greetings of the day
             </Typography>
             <Typography variant="h5" component="h2">
-              How are you ?
+              500$
             </Typography>
             <Typography
               style={{
@@ -137,13 +195,13 @@ return (
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Stay Safe.....</Button>
+            <Button size="small">Add to Card</Button>
           </CardActions>
         </Card>
         
         </div>
       </Grid>
-      {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+      {/* ++===============================================================*/}
       <Grid item xs={6} sm={3}>
       <div style={{}}>
         <Card
@@ -162,7 +220,7 @@ return (
               Greetings of the day
             </Typography>
             <Typography variant="h5" component="h2">
-              How are you ?
+              600$
             </Typography>
             <Typography
               style={{
@@ -176,12 +234,12 @@ return (
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Stay Safe.....</Button>
+            <Button size="small">Add to Card</Button>
           </CardActions>
         </Card>
         
         </div>
-        {/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
+        {/* ===============================================================*/}
       </Grid>
 
     </Grid>
